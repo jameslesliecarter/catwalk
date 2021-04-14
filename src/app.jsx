05f49a001eb4bea11ac8a2d9f6f2ed909b27ee53
David Hepburn <redcaryellowbus@gmail.com>
2021-04-14 19:23:55 -0400

    upping changes to config files

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   constructor(props) {
@@ -6,9 +6,7 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>hi</div>
-    )
+    return <div>hi</div>;
   }
 }
 
