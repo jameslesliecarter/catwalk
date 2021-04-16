32db92044b66cffffa5899f5ad690fad1d19722a
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-16 15:32:00 -0400

    created skeleton files for these, no work done on them yet

---
+++ src/Related/components/CardsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from '../../Widgets/Carousel.jsx';
+
+const CardsList = () => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  )
+}
+
+export default CardsList;
