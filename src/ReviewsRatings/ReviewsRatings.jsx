78a1af7aaab257ef23bcbb2dd246700efc21b555
David Hepburn <redcaryellowbus@gmail.com>
2021-04-16 15:31:52 -0400

    renamed my top-level component & folder

---
+++ src/ReviewsRatings/ReviewsRatings.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ax from 'axios';
+
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="reviews-ratings">
+      </div>;
+    );
+  }
+}
+
+export default Review;
