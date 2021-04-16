4d4f5c7548f7b9786e2df50877c9dd9a8447cf97
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-16 19:16:00 -0400

    converted Carousel to class component with state for arrow clicks

--- src/Widgets/Carousel.jsx
+++ src/Widgets/Carousel.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
 
-const Carousel = () => {
-  return (
-    <div>hihihihihihihih</div>
-  )
-};
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImg: 0
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    var clickedClass = e.target.classList.value;
+    if (clickedClass === 'arrow-left' && this.state.currentImg !== 0) {
+      this.setState({currentImg: this.state.currentImg - 1});
+    } else if (clickedClass === 'arrow-right' && this.state.currentImg !== this.props.images.length) {
+      this.setState({currentImg: this.state.currentImg + 1});
+    }
+  }
+
+  render() {
+    return (
+      <section className="slider">
+        <FaArrowAltCircleLeft className="arrow-left" onClick={this.handleClick} />
+        {this.props.images.map((image, index) => <img src={image.image} alt="travel" />)}
+        <FaArrowAltCircleRight className="arrow-right" onClick={this.handleClick} />
+      </section>
+    )
+  }
+}
 
 export default Carousel;
