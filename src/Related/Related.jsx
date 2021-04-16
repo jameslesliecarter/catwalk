4d4a039738072ce131477528c3c51bdadf20f74a
David Hepburn <redcaryellowbus@gmail.com>
2021-04-16 15:29:42 -0400

    cleaned up local git

---
+++ src/Related/Related.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Related extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>hi</div>;
+  }
+}
+
+export default Related;
---
+++ src/Reviews/Reviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ax from 'axios';
+
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>hi</div>;
+    );
+  }
+}
+
+export default Review;
