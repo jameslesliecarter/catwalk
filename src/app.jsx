e5b4cb3adfc368ff5da6ef31a207f7c6aa479288
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-19 11:24:01 -0400

    added Navbar component and removed unnecessary divs

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from './Navbar/Navbar.jsx'
 import Overview from "./Overview/Overview.jsx";
 import QandA from "./QandA/QandA.jsx";
 import Related from "./Related/Related.jsx";
@@ -12,18 +13,11 @@ class App extends React.Component {
   render() {
     return (
     <div>
-      <div>
+      <Navbar />
       <Overview />
-      </div>
-      <div>
-        <QandA />
-      </div>
-      <div>
-        <Related />
-      </div>
-      <div>
-        <Reviews />
-      </div>
+      <Related />
+      <QandA />
+      <Reviews />
     </div>
     );
   }
