a097c68bcb0eeb0f6cd37ba1d15147f2e72950ae
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-19 09:30:30 -0400

    refactored arrow pointers to not show on first image and last image appropriately

--- src/Widgets/Carousel.jsx
+++ src/Widgets/Carousel.jsx
@@ -8,22 +8,32 @@ class Carousel extends React.Component {
       currentImg: 0
     }
 
-    this.handleClick = this.handleClick.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+    this.prevSlide = this.prevSlide.bind(this);
+    this.nextSlide = this.nextSlide.bind(this);
   }
 
-  handleClick(e) {
-    var clickedClass = e.target.classList.value;
-    if (clickedClass === 'arrow-left' && this.state.currentImg !== 0) {
-      this.setState({currentImg: this.state.currentImg - 1});
-    } else if (clickedClass === 'arrow-right' && this.state.currentImg < this.props.images.length - 1) {
-      this.setState({currentImg: this.state.currentImg + 1});
-    }
+  // handleClick(e) {
+  //   var clickedClass = e.target.classList.value;
+  //   if (clickedClass === 'arrow-left' && this.state.currentImg !== 0) {
+  //     this.setState({currentImg: this.state.currentImg - 1});
+  //   } else if (clickedClass === 'arrow-right' && this.state.currentImg < this.props.images.length - 1) {
+  //     this.setState({currentImg: this.state.currentImg + 1});
+  //   }
+  // }
+
+  prevSlide() {
+    this.setState({currentImg: this.state.currentImg - 1});
+  }
+
+  nextSlide() {
+    this.setState({currentImg: this.state.currentImg + 1});
   }
 
   render() {
     return (
       <section className="slider">
-        <FaArrowAltCircleLeft className="arrow-left" onClick={this.handleClick} />
+        {this.state.currentImg === 0 ? <></> : <FaArrowAltCircleLeft className="arrow-left" onClick={this.prevSlide} />}
         {this.props.images.map((image, index) => {
           return (
             <div className={index === this.state.currentImg ? 'slide active' : 'slide'} key={index}>
@@ -31,7 +41,7 @@ class Carousel extends React.Component {
             </div>
           )
         })}
-        <FaArrowAltCircleRight className="arrow-right" onClick={this.handleClick} />
+        {this.state.currentImg === this.props.images.length - 1? <></> : <FaArrowAltCircleRight className="arrow-right" onClick={this.nextSlide} />}
       </section>
     )
   }
