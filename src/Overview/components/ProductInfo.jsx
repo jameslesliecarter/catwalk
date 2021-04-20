36918ba6aad8b052685d8841cda4627116e37005
Jake Nalls <jakenalls@gmail.com>
2021-04-20 16:57:36 -0400

    imported star icons and added to product info component

--- src/Overview/components/ProductInfo.jsx
+++ src/Overview/components/ProductInfo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import StarRating from '/src/Widgets/StarRating.jsx'
 
 const ProductInfo = ({products}) => {
   return (
     <div className='overview-widget-productInfo'>
-      <div>Star Rating <href>See all reviews</href></div>
+      <div><StarRating/> <href>See all reviews</href></div>
       <p>{products[0].category}</p>
       <h4>{products[0].name}</h4>
       <p>{products[0].default_price}</p>
---
+++ src/Widgets/StarRating.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {FaRegStar} from 'react-icons/fa';
+
+class StarRating extends React.Component {
+  render() {
+    return <h3><FaRegStar/><FaRegStar/><FaRegStar/><FaRegStar/><FaRegStar/></h3>
+  }
+}
+
+export default StarRating
