be0d75203f54a87efddfc5dc576d92d82ff9e3f8
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-20 07:24:36 -0400

    added test axios method

--- src/app.jsx
+++ src/app.jsx
@@ -4,10 +4,17 @@ import Overview from "./Overview/Overview.jsx";
 import Related from "./Related/Related.jsx";
 import QandA from "./QandA/QandA.jsx";
 import Reviews from "./Reviews/Reviews.jsx";
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:9000/products/19093')
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
   }
 
   render() {
