c65a06e45ef6821760dc69de09beeebf152d8b37
David Hepburn <redcaryellowbus@gmail.com>
2021-04-20 19:28:47 -0400

    had to included git rm'd files

---
+++ server/routes/questions.js
@@ -0,0 +1,99 @@
+// Questions REQs ============================================================ //
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+let apiKey = require('../.apiKey.js');
+
+// create axios instance
+let ax = axios.create({
+  baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions',
+  timeout: 1000,
+  headers: apiKey
+});
+
+// middleware applied to all REQs
+router.use((req, res, next) => {
+  // Ternary operator to use page and count on condition that
+  // they are provided. Otherwise use server default.
+  req.page = req.query.page ? `&page=${req.query.page}` : '';
+  req.count = req.query.count ? `&count=${req.query.count}` : '';
+  req.product_id = req.query.product_id || null;
+  next();
+})
+
+
+router.route('/')
+  .get((req, res, next) => {
+    ax.get(`/?product_id=${req.product_id}${req.page}${req.count}`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.error('\n/questions/ ax error:\n', error);
+      res.end('error in /:product_id');
+    });
+  })
+  .post((req, res, next) => {
+    ax.post('/', req.body)
+    .then((response) => {
+      console.log('Question was created: ', response.data === "Created");
+      res.end();
+    })
+    .catch((error) => {
+      console.error('POST ERROR TO HEROKU: ', error)
+    });
+    res.end();
+  });
+
+router.route('/:question_id/answers')
+  .get((req, res, next) => {
+    ax.get(`/${req.params.question_id}/answers/?${req.page}&${req.count}`)
+    .then((response) => {
+      res.send(response.data.results);
+      res.end();
+    })
+    .catch((error) => {
+      console.error('Answer GET error: ', error);
+      res.end();
+    });
+  })
+  .post((req, res, next) => {
+    ax.post(`/${req.body.question_id}/answers/`, req.body)
+    .then((response) => {
+      console.log('We\'ve been created, y\'all: ', response.data === 'Created');
+      res.end();
+    })
+    .catch((error) => {
+      console.error('Answer not created. Error: ', error);
+      res.end();
+    });
+  });
+
+router.route('/:question_id/report')
+  .put((req, res, next) => {
+    ax.put(`/${req.body.question_id}/report`)
+    .then((response) => {
+      console.log('We\'ve successfully reported the question ', response.status === 204);
+      res.end();
+    })
+    .catch((error) => {
+      console.error('Issue reporting question: ', error);
+      res.end();
+    });
+  });
+
+router.route('/:question_id/helpful')
+  .put((req, res, next) => {
+    ax.put(`/${req.body.question_id}/helpful`)
+    .then((response) => {
+      console.log('We\'ve created a helpful input: ', response.status === 204);
+      res.end();
+    })
+    .catch((error) => {
+      console.error('You weren\'t very helpful, were you: ', error);
+      res.end();
+    });
+  });
+
+
+module.exports = router;
---
+++ src/QandA/QandA.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import axios from "axios";
+import Search from './components/Search.jsx';
+import QuestionList from './components/QuestionList.jsx';
+import MoreQuestions from './components/MoreQuestions.jsx';
+import AddQuestion from './components/AddQuestion.jsx';
+
+import sampleData from '../../SampleData.js';
+
+
+class QandA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [],
+      product_id: ''
+    }
+    this.fetchQuestions = this.fetchQuestions.bind(this);
+    this.postQuestion = this.postQuestion.bind(this);
+  }
+
+
+  fetchQuestions() {
+    axios.get('qa/questions/?product_id=19093&page=1&count=30')
+    .then((data) => {
+      this.setState({
+        questions: data.data.results
+      });
+    })
+    .catch((err) => {
+      console.log('QA ERROR: ', err);
+    });
+  }
+
+  postQuestion(name, email, body, product_id) {
+    axios.post('/qa/questions', {
+      name: name,
+      email: email,
+      body: body,
+      product_id: product_id
+    })
+    .then((response) => {
+      console.log('hey it works')
+    })
+    .catch((response) => {
+      console.log('Not so moovin, ', error);
+    });
+  }
+
+  componentDidMount() {
+    this.fetchQuestions();
+    // this.postQuestion();
+  }
+  render() {
+    return (
+      <div className="q-and-a-widget">
+        <div>
+          <Search />
+        </div>
+        <div className="questions-and-answers">
+          <QuestionList questions={this.state.questions} />
+        </div>
+        <div className="more-questions">
+          <MoreQuestions />
+        </div>
+        <div className="add-question">
+          <AddQuestion />
+        </div>
+      </div>
+    )
+  }
+}
+export default QandA;
