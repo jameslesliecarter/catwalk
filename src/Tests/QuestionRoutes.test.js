783494f19909971469ee88a87c9b29d16be135f4
Jim Carter <jameslesliecarter@gmail.com>
2021-04-21 11:43:50 -0400

    First pass api testing functional.

--- src/Tests/QuestionRoutes.test.js
+++ src/Tests/QuestionRoutes.test.js
@@ -1,12 +1,13 @@
 const app = require('../../server/index.js');
 const supertest = require('supertest');
 const request = supertest(app);
+import 'regenerator-runtime/runtime';
 
 describe('Testing Q and A endpoints', () => {
   it('Gets the QandA test endpoint', async done => {
-    const res = await request.get('/qa/questions/test')
-    expect(response.status).toBe(200)
-    expect(response.body.message)toBe('pass!')
+    const response = await request.get('/qa/questions/test');
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('pass!');
     done();
   });
 });
