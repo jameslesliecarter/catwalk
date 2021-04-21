807e78738983157a1ea03110ae7db55e1982d726
Jake Nalls <jakenalls@gmail.com>
2021-04-21 16:33:54 -0400

    added slider.jsx rendering a list carousel with hard coded state in overview

--- src/Widgets/ListCarousel.jsx
+++ src/Widgets/ListCarousel.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Card from '../Related/components/Card.jsx';
-import Arrow from './Arrow.jsx';
-import {RiArrowRightSLine, RiArrowLeftSLine} from 'react-icons/ri';
+// import Arrow from './Arrow.jsx';
+// import {RiArrowRightSLine, RiArrowLeftSLine} from 'react-icons/ri';
+import Slider from './Slider.jsx';
 
 class ListCarousel extends React.Component {
   constructor(props) {
@@ -15,15 +16,20 @@ class ListCarousel extends React.Component {
   }
 
   render() {
+    const offsetWidthValue = this.refs.offsetWidth,
+    scrollWidthValue = this.refs.scrollWidth;
+
     return (
-      <section className="carousel carousel-list">
-        <Arrow direction='left' arrowClick={this.arrowClick} glyph={<RiArrowLeftSLine />} />
-        {this.props.images.map((image, index) => {
-          return (
-            <Card key={index} image={image.thumbnail_url} imgClick={this.props.imgClick}/>
-          )
-        })}
-        <Arrow direction='right' arrowClick={this.arrowClick} glyph={<RiArrowRightSLine />} />
+      <section className="carousel carousel-list parent">
+        {/* <Arrow direction='left' arrowClick={this.arrowClick} glyph={<RiArrowLeftSLine />} /> */}
+        <Slider>
+          {this.props.images.map((image, index) => {
+            return (
+              <Card key={index} image={image.thumbnail_url} imgClick={this.props.imgClick}/>
+            )
+          })}
+        </Slider>
+        {/* <Arrow direction='right' arrowClick={this.arrowClick} glyph={<RiArrowRightSLine />} /> */}
       </section>
     )
   }
