d1f58b4e0195ea33f97393ee253da0de5a2b2813
Jim Carter <jameslesliecarter@gmail.com>
2021-04-21 22:58:34 -0400

    Loading answers based on question id

--- src/QandA/components/QuestionItem.jsx
+++ src/QandA/components/QuestionItem.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import AnswerList from './AnswerList.jsx';
 
-const QuestionItem = (props) => {
+const QuestionItem = ({question}) => {
   return (
-    <div className="question-item">
-      <div className="question-body">
-        <span className="question-icon">Q: </span>{props.question.question_body}
-      </div>
-      <div className="question-interaction interaction">
-        <div className="helpful-btn btn">
-          Helpful btn
+    <div className="question-and-answer-container">
+      <div className="question-item">
+        <div className="question-body">
+          <span className="question-icon">Q: </span>{question.  question_body}
         </div>
-        <div className="add-answer-btn btn">
-          Add Answer btn
+        <div className="question-interaction interaction">
+          <div className="helpful-btn btn">
+            Helpful btn
+          </div>
+          <div className="add-answer-btn btn">
+            Add Answer btn
+          </div>
         </div>
       </div>
+      <AnswerList question={question} />
     </div>
   );
 };
--- src/QandA/components/QuestionList.jsx
+++ src/QandA/components/QuestionList.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import QuestionItem from './QuestionItem.jsx';
-import AnswerList from './AnswerList.jsx';
+
 
 const QuestionList = ({questions}) => {
   return (
     <div>
       {questions.map((question, i) => {
         return (
-        <div key={i}>
-          <div className="question-container container">
+          <div key={i} className="question-container container">
             <QuestionItem key={i} question={question}/>
           </div>
-          <div className="answer-container container">
-            {Object.keys(question.answers).map((k,j) => {
-              return <AnswerList answer={question.answers[k]} key={j} />
-            })}
-          </div>
-        </div>
       )})}
     </div>
   );
 };
+
 export default QuestionList;
