f366c5502a476d2fee6c9a66ba75945fd376a99f
David Hepburn <redcaryellowbus@gmail.com>
2021-04-23 15:58:54 -0400

    component

--- src/Reviews/components/ReviewListItem.jsx
+++ src/Reviews/components/ReviewListItem.jsx
@@ -14,11 +14,24 @@ const ReviewListItem = (props) => {
     photos
   } = props.review;
   //console.dir(photos);
+  date = new Date(Date.parse(date)).toLocaleString('en-US',{dateStyle:'long'});
   return (
     <div id={review_id}>
       <span className="list-item__star-rating">*****({rating})</span>
       <span className="list-item__reviewer-name">{reviewer_name}</span>
       <span className="list-item__date">{date}</span>
+      <h4>{summary}</h4>
+      <p>{body}</p>
+      { (response) ?
+        <div>
+          <span><p><b>Response:</b></p></span>
+          <span><p>{response}</p></span>
+        </div>
+        : null
+      }
+      <p>Helpful?</p>
+      <a><u>Yes</u> | <u>Report</u></a>
+      <hr />
     </div>
   );
 };
