daaca557b9a5ea79ea1a54cc48a5e36bb32b7c8b
David Hepburn <redcaryellowbus@gmail.com>
2021-04-24 08:33:16 -0400

    working on dropdown

--- src/Reviews/components/ReviewsOverview.jsx
+++ src/Reviews/components/ReviewsOverview.jsx
@@ -3,13 +3,15 @@ import Dropdown from '../../Widgets/Dropdown.jsx'
 
 const ReviewsOverview = (props) => {
   let totalReviews = props.totalReviews;
+  let changeSort = props.changeSort;
   return (
     <div className="reviews-overview">
       {totalReviews} reviews, sorted by
       <Dropdown
         ddLabel="ddReviews"
-        ddList={['relevance', 'latest', 'helpfulness']}
-        ddDefault="relevance"
+        ddList={['relevant', 'newest', 'helpful']}
+        ddVal="relevant"
+        ddCB={changeSort}
       />
     </div>
   );
