1a78722953e3231bff7f41118b5a3949764d6552
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-27 15:14:13 -0400

    completed table modal functionality

--- src/Related/Comparison.jsx
+++ src/Related/Comparison.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import _ from 'underscore';
+import {GiCheckMark} from 'react-icons/gi';
 
 const Comparison = ({currentProduct, comparedProduct}) => {
-  console.log('current product ', currentProduct);
-  console.log('compared product ', comparedProduct);
+  let currentFeatures = currentProduct.features;
+  let comparedFeatures = comparedProduct.features;
+
+  let allUniqFeatures = _.uniq(currentFeatures.concat(comparedFeatures), _.property('value')).map(feature => {
+    feature.compared = comparedFeatures.some(item => item.value === feature.value && item.feature === feature.feature);
+    feature.current = currentFeatures.some(item => item.value === feature.value && item.feature === feature.feature);
+    return feature;
+  });
+
   return (
    <table>
      <tr>
@@ -10,6 +19,15 @@ const Comparison = ({currentProduct, comparedProduct}) => {
        <th/>
        <th>{comparedProduct.name}</th>
      </tr>
+     {allUniqFeatures.map(feature => {
+       return (
+         <tr>
+           <th>{feature.current ? <GiCheckMark /> : <></>}</th>
+           <th>{feature.feature + ': ' + feature.value}</th>
+           <th>{feature.compared ? <GiCheckMark /> : <></>}</th>
+         </tr>
+       )
+     })}
    </table>
   )
 }
