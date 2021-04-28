f8f44d5fcd8d39b510c73924d5c3ffd6ac6e2578
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-28 05:33:14 -0400

    removed search component to a widget

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { FaSearch } from 'react-icons/fa';
+import Search from '../Widgets/Search.jsx';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <img src="https://symplmarketing.com/wp-content/uploads/2019/02/sympl-2.png" alt="logo" className="logo"/>
-      <div className="search">
-        <input className="search-input" placeholder="Search products..." />
-        <button className="btn search-btn"><FaSearch className="search-icon" /></button>
-      </div>
+      <Search type='text' placeholder='Search' onClick={() => console.log('hi')}/>
     </nav>
   )
 }
