92407d70ad9658ca1c4106450d5140bbc66204fb
Ruben <ruben@rubens-MacBook-Pro.local>
2021-04-30 01:20:51 -0400

    added styling to qa

--- src/QandA/components/MoreQuestions.jsx
+++ src/QandA/components/MoreQuestions.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MoreQuestions = ({revealQuestions}) => {
   return (
-    <div onClick={revealQuestions} className="more-questions-btn">
-      More Questions
+    <div onClick={revealQuestions} className="qa-more-btn">
+      MORE ANSWERED QUESTIONS
     </div>
   )
 };
